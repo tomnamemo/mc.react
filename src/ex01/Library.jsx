046841ef3ca72433308c33dc22d6906eb028bb6a
tomnamemo <tomna.memo@gmail.com>
2024-10-28 18:12:34 +0900

    connect repository

---
+++ src/ex01/Library.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Book from "./book";
+function Library(props) {
+	return (
+		<div>
+			<Book
+				name="코딩 좀 아는 사람"
+				numOfPage={312}
+			/>
+			<Book
+				name="Java 언어로 배우는 디자인 패턴 입문"
+				numOfPage={560}
+			/>
+			<Book
+				name="계속 가봅시다 남는 게 체력인데"
+				numOfPage={284}
+			/>
+		</div>
+	);
+}
+export default Library;
